6a24c594f157b6edec1058efab8bc21ea92532aa
compscidavid <48367000+compscidavid@users.noreply.github.com>
2021-02-07 19:28:42 -0500

    Add files via upload

---
+++ Token.java
@@ -0,0 +1,16 @@
+package application;
+
+
+public class Token {
+	public String tokenType;
+	public String tokenValue;
+	
+	public Token(String tokenType, String tokenValue) {
+		this.tokenType = tokenType;
+		this.tokenValue = tokenValue;
+	}
+	
+	public String toString() {
+		return (tokenValue);
+	}
+}
